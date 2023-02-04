6c84cb52404dd5bdde540cf49cfa3d88e4280374
Bell Sarian <37950541+bell-sarian@users.noreply.github.com>
2023-02-03 17:16:30 -0800

    add NavBar begin first art
    Created NavBar component
    
    Created OceanSunset art coponent and began adding color blocks

---
+++ src/Art/OceanSunset/OceanSunset.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Styles from "./OceanSunset.css";
+
+
+export default function OceanSunset()  {
+    return (
+      <div className="ocean-sunset-container">
+        <div className="wave-1-1"></div>
+        <div className="wave-2-1"></div>
+        <div className="wave-3-1"></div>
+        <div className="wave-4-1"></div>
+      </div>
+    );
+  
+}
