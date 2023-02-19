21bdf39fce7d21cc07f5a8c2bc1196aefd0f4927
Bell Sarian <37950541+bell-sarian@users.noreply.github.com>
2023-02-18 19:58:38 -0800

    Add animated stars
    Add:
    - animated stars in star component
    - adjust block lumocity depending on horizon vs ocean

---
+++ src/Art/OceanSunset/Stars/Stars.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import styles from './Stars.scss';
+
+// Twinkling Night Sky by Sharna
+
+// Random Stars
+export default function Stars() {
+
+    const [windowSize, setWindowSize] = useState([
+        window.innerWidth,
+        window.innerHeight,
+    ]);
+    const domContent = [
+        
+    ];
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+        setWindowSize([window.innerWidth, window.innerHeight]);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+        window.removeEventListener('resize', handleWindowResize);
+        };
+    });
+
+    for (let i = 0 ; i < 70; i++) {
+        var xposition = Math.floor(Math.random() * window.innerWidth);
+        var yposition = Math.floor(Math.random() * window.innerHeight);
+        var star_type = Math.floor((Math.random() * 3) + 1);
+        var position = {
+            "x" : window.innerWidth * xposition,
+            "y" : window.innerHeight * yposition,
+        };
+        domContent.push({"type": star_type, "yAxis": yposition, "xAxis": xposition})
+    }
+    
+
+    return(
+        <div className="galaxy">
+            {domContent.map((item, index) => {
+                var xposition = Math.floor(Math.random() * window.innerWidth);
+                var yposition = Math.floor(Math.random() * window.innerHeight);
+                var star_type = Math.floor((Math.random() * 3) + 1);
+
+                console.log(xposition + " + " + yposition)
+                console.log("star_type" + star_type)
+                return (
+                <div className={"star star-type" + item.type} style={{ top : item.yAxis , left : item.xAxis}} ></div>
+                )
+            })}
+        </div>
+        
+    )
+    
+};
+
