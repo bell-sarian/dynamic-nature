89375a76c27169c05992ccc432a004dabef6383b
Bell Sarian <37950541+bell-sarian@users.noreply.github.com>
2023-03-03 23:13:11 -0800

    Add logo and change selection color

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,12 @@
 import React, {  useState } from "react";
 import Styles from "./NavBar.scss";
 import GridIcon from "../../Assets/grid_icon.svg";
+import BWGF3 from "../../Assets/BW-GF3.svg";
 import GridMenu from "../GridMenu/GridMenu";
 import MenuOverlay from "../GridMenu/MenuOverlay/MenuOverlay";
 import { color } from "@mui/system";
 import { Link } from "react-router-dom";
+
 
 
 export default function NavBar()  {
@@ -14,11 +16,13 @@ export default function NavBar()  {
         className="menu-bar-container" 
         >
         <div className="menu-about">
-            <div>Bell'Aria S.W.</div>
+        <Link to="/" className="menu-web-name-link" >
+        <img src={BWGF3} alt="GF Cubed Icon" style={{color: "#E1C597"}}/>
+        </Link>
         </div>
-        <div className="menu-web-name">
+        {/* <div className="menu-web-name">
           <Link to="/" className="menu-web-name-link" >DYNAMIC NATURE</Link>
-        </div>
+        </div> */}
         
 
       <div className="nav-menu-grid-button">
